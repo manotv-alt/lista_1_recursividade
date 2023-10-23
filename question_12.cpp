dd4284ff64fd630adf24272decf24ba58ac65984
Nuelzin <manotvloco0@gmail.com>
2023-10-23 14:57:30 -0300

    att

--- question_12.cpp
+++ question_12.cpp
@@ -9,18 +9,21 @@ typedef struct {
 } coordenadas;
 
 void lab (vector<vector<coordenadas>> &labirinto, vector<vector<bool>> &visitados, int linha, int coluna, int *resultado) {
+    
     if(visitados[linha][coluna] == true) {
         *resultado = 2;
         return;
     }
-    if(labirinto[linha][coluna].x == 0 and labirinto[linha][coluna].y == 0) {
+    if(linha == 0 and coluna == 0) {
         *resultado = 1;
         return;
     }
 
-    linha = labirinto[linha][coluna].y;
-    coluna = labirinto[linha][coluna].x;
     visitados[linha][coluna] = true;
+    int ax = linha;
+    linha = labirinto[ax][coluna].y;
+    coluna = labirinto[ax][coluna].x;
+    
         
     lab(labirinto, visitados, linha, coluna, resultado);
 }
@@ -40,7 +43,8 @@ int main () {
             cin >> labirinto[i][j].y >> labirinto[i][j].x;
         }
     }
-    
+
+
     cin >> y_inicial >> x_inicial;
 
     lab(labirinto, visitados, y_inicial, x_inicial, &resultado);
